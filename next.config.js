e76dd72a825da4ac9df6c11e4252b2123a655bd6
rajaHazwan <rajahazwan02@gmail.com>
2026-03-31 16:32:09 +0800

    fix: migrate next.config.js for Next.js 16 Turbopack compatibility

--- next.config.js
+++ next.config.js
@@ -2,18 +2,16 @@
 
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.pdf$/i,
-      type: "asset/source",
-    });
-
-    return config;
-  },
+  turbopack: {},
   env: {
     BASE_URL: process.env.BASE_URL,
   },
   images: {
-    domains: ["img.shields.io", "pohwp.dev", "www.pohwp.dev"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.shields.io",
+      },
+    ],
   },
 };
